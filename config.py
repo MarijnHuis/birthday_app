975b8a9ab61d83134749844f9c6c1b9f688e176d
Marijn <marijn2huis@gmail.com>
2024-08-11 18:05:42 +0100

    rewritten to use WTForms and added some error pages

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "birthdays.db")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
